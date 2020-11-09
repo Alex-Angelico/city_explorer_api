410f2d37d19909587319482a6a4042e4cdc39b5d
Jordan Kidwell <jkid2023@gmail.com>
2020-11-09 13:47:09 -0800

    added in location function

--- server.js
+++ server.js
@@ -13,9 +13,31 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.static('./public'));
 
-app.get('/about-us', (request, response) => {
-  response.send('this is the about us webpage');
+app.get('/about-us', (req, res) => {
+  res.send('this is the about us webpage');
 });
+function handleLocation(req, res) {
+  try {
+    let rawLocationData = require('./data/location.json');
+    let city = req.query.city;
+    let locationData = new Location(city, rawLocationData);
+    res.send(locationData);
+  } catch(error){
+    console.error(error);
+  }
+}
+
+function Location(city, rawLocationData) {
+  this.rawDataSearchQuery = city;
+  this.formattedSearchQuery = rawLocationData[0].display_name;
+  this.latitude = rawLocationData[0].lat;
+  this.longitude = rawLocationData[0].lon;
+}
+
+
+
+
+app.get('/location', handleLocation);
 
 app.listen(PORT, () => {
   console.log(`server up on port ${PORT}`);
