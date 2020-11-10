efb8cadec14bb1fc90f99a4532cf32bbd06310b7
Alex Angelico <alex.angelico@live.com>
2020-11-10 15:31:50 -0800

    refactored handleLocation() with external API call

--- server.js
+++ server.js
@@ -1,55 +1,58 @@
 'use strict';
 
-require('dotenv').config();
-
-
 const express = require('express');
+const superagent = require('superagent');
+const dotenv = require('dotenv');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 
 app.use(cors());
 app.use(express.static('./public'));
 
+app.get('/location', handleLocation);
+app.get('/weather', handleWeather);
+
+
+
+// function handleLocation(req, res) {
+//   try {
+//     let rawLocationData = require('./data/location.json');
+//     let city = req.query.city;
+//     let locationData = new Location(city, rawLocationData);
+//     res.send(locationData);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
 function handleLocation(req, res) {
-  try {
-    let rawLocationData = require('./data/location.json');
-    let city = req.query.city;
-    let locationData = new Location(city, rawLocationData);
-    res.send(locationData);
-  } catch (error) {
-    console.error(error);
-  }
-}
+  let city = req.query.city;
+  let url = `http://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
+  let locations = {};
 
-function handleReq(req, res) {
-  Let searchVar = req.query.searchVar;
-  Let url = `[extrnal API URL]?key=${API KEY
-}& q=${ city }& `;
-	Let cachedDataSet = {};
-	
-	if (locations[url]) {
-		res.send(locations[url]);
-	} else {
-	  superperagent.get(url)
-		.then(data => {
-		const rawData = data.body-0];
-		const returnData = new ReturnObjectConstructor(searchVar, apiObjectData)
-		cachedDataSet[url] = returnData
-		res.json(returnData);
-	})
-	.catch(() => {
-	  console.error('did not work');
-	})
-    }
+  if (locations[url]) {
+    res.send(locations[url]);
+  } else {
+    superagent.get(url)
+      .then(data => {
+        const geoData = data.body[0];
+        const locationData = new Location(city, geoData);
+        locations[url] = locationData;
+        res.json(locationData);
+      })
+      .catch(() => {
+        console.error('did not work');
+      })
+  }
 }
 
 function handleWeather(req, res) {
   try {
     let rawWeatherData = require('./data/weather.json');
-    let city = `${ req.query.city } 5 - day: `;
+    // let city = `${req.query.city} 5 - day: `;
     let array = rawWeatherData.data;
     Weather.all = array.map(object => new Weather(object));
     // let weatherReport = `${ city } ${ Weather.all } `;
@@ -72,13 +75,12 @@ function Weather(object) {
   this.time = object.datetime;
 }
 
-app.get('/location', handleLocation);
-app.get('/weather', handleWeather);
+
 
 app.use('*', (req, res) => {
   res.status(500).send('Sorry, something went wrong!');
 })
 
 app.listen(PORT, () => {
-  console.log(`server up on port ${ PORT } `);
+  console.log(`server up on port ${PORT} `);
 });
